77354e34087c547affe1b098d2ac59d7864af4cc
nexoscreator <112468782+nexoscreator@users.noreply.github.com>
2025-02-22 10:49:48 +0530

    chore: update github pages

--- api/config.js
+++ api/config.js
@@ -1,6 +1,1 @@
-const config = {
-  apiKey: 'YOUR_API_KEY_HERE', // Replace 'YOUR_API_KEY_HERE' with your actual API key
-  apiUrl: 'https://api.spoonacular.com/recipes', // Spoonacular API base URL
-};
-
-export { config };
+let config={apiKey:"9cd33a0a6ef14a9bb615d282147de383",apiUrl:"https://api.spoonacular.com/recipes"};export{config};
